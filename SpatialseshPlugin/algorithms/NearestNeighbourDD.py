78f9f928ebc7127dd14721139a29b2e9db154a5e
Teodora Koleva <tedi20021810@gmail.com>
2026-07-09 16:49:39 +0300

    variables edits and typing configuration

--- SpatialseshPlugin/algorithms/NearestNeighbourDD.py
+++ SpatialseshPlugin/algorithms/NearestNeighbourDD.py
@@ -70,8 +70,8 @@ class NearestNeighbourDDAlgorithm(QgsProcessingAlgorithm):
         feedback: QgsProcessingFeedback | None,
     ) -> dict[str, Any]:
         feedback = QgsProcessingMultiStepFeedback(9, feedback)
-        results = {}
-        outputs = {}
+        results: dict[str, Any] = {}
+        outputs: dict[str, Any] = {}
 
         # Join attributes by nearest
         outputs["JoinAttributesByNearest"] = processing.run(
@@ -90,6 +90,8 @@ class NearestNeighbourDDAlgorithm(QgsProcessingAlgorithm):
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["JoinAttributesByNearest"] is not None
 
         feedback.setCurrentStep(1)
         if feedback.isCanceled():
@@ -113,141 +115,167 @@ class NearestNeighbourDDAlgorithm(QgsProcessingAlgorithm):
             is_child_algorithm=True,
         )
 
+        assert outputs["FieldCalculatorAzimuth"] is not None
+
         feedback.setCurrentStep(2)
         if feedback.isCanceled():
             return {}
+        feedback.setProgressText("Azimuth calculated.")
 
         # Field calculator - bearing
-        alg_params = {
-            "FIELD_LENGTH": 0,
-            "FIELD_NAME": "Bearing",
-            "FIELD_PRECISION": 0,
-            "FIELD_TYPE": 2,  # Text (string)
-            "FORMULA": "case when \"Azimuth\" >337.5 OR \"Azimuth\" <22.5 then 'North' else '' end +\r\ncase when \"Azimuth\" >22.5 AND \"Azimuth\" <67.5 then 'North-East' else '' end +\r\ncase when \"Azimuth\" >67.5 AND \"Azimuth\" <112.5 then 'East' else '' end +\r\ncase when \"Azimuth\" >112.5 AND \"Azimuth\" <157.5 then 'South-East' else '' end +\r\ncase when \"Azimuth\" >157.5 AND \"Azimuth\" <202.5 then 'South' else '' end +\r\ncase when \"Azimuth\" >202.5 AND \"Azimuth\" <247.5 then 'South-West' else '' end +\r\ncase when \"Azimuth\" >247.5 AND \"Azimuth\" <292.5 then 'West' else '' end +\r\ncase when \"Azimuth\" >292.5 AND \"Azimuth\" <337.5 then 'North-West' else '' end",
-            "INPUT": outputs["FieldCalculatorAzimuth"]["OUTPUT"],
-            "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
-        }
         outputs["FieldCalculatorBearing"] = processing.run(
             "native:fieldcalculator",
-            alg_params,
+            {
+                "FIELD_LENGTH": 0,
+                "FIELD_NAME": "Bearing",
+                "FIELD_PRECISION": 0,
+                "FIELD_TYPE": 2,  # Text (string)
+                "FORMULA": "case when \"Azimuth\" >337.5 OR \"Azimuth\" <22.5 then 'North' else '' end +\r\ncase when \"Azimuth\" >22.5 AND \"Azimuth\" <67.5 then 'North-East' else '' end +\r\ncase when \"Azimuth\" >67.5 AND \"Azimuth\" <112.5 then 'East' else '' end +\r\ncase when \"Azimuth\" >112.5 AND \"Azimuth\" <157.5 then 'South-East' else '' end +\r\ncase when \"Azimuth\" >157.5 AND \"Azimuth\" <202.5 then 'South' else '' end +\r\ncase when \"Azimuth\" >202.5 AND \"Azimuth\" <247.5 then 'South-West' else '' end +\r\ncase when \"Azimuth\" >247.5 AND \"Azimuth\" <292.5 then 'West' else '' end +\r\ncase when \"Azimuth\" >292.5 AND \"Azimuth\" <337.5 then 'North-West' else '' end",
+                "INPUT": outputs["FieldCalculatorAzimuth"]["OUTPUT"],
+                "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
+            },
             context=context,
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["FieldCalculatorBearing"] is not None
 
         feedback.setCurrentStep(3)
         if feedback.isCanceled():
             return {}
+        feedback.setProgressText("Bearing calculated.")
 
         # Retain fields
-        alg_params = {
-            "FIELDS": QgsExpression(
-                "'DesignationType;Designation;SiteName;distance;Bearing;Azimuth'"
-            ).evaluate(),
-            "INPUT": outputs["FieldCalculatorBearing"]["OUTPUT"],
-            "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
-        }
         outputs["RetainFields"] = processing.run(
             "native:retainfields",
-            alg_params,
+            {
+                "FIELDS": QgsExpression(
+                    "'DesignationType;Designation;SiteName;distance;Bearing;Azimuth'"
+                ).evaluate(),
+                "INPUT": outputs["FieldCalculatorBearing"]["OUTPUT"],
+                "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
+            },
             context=context,
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["RetainFields"] is not None
 
         feedback.setCurrentStep(4)
         if feedback.isCanceled():
             return {}
+        feedback.setProgressText("Retained only required fields.")
 
         # Order by expression
-        alg_params = {
-            "ASCENDING": True,
-            "EXPRESSION": "distance",
-            "INPUT": outputs["RetainFields"]["OUTPUT"],
-            "NULLS_FIRST": False,
-            "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
-        }
         outputs["OrderByExpression"] = processing.run(
             "native:orderbyexpression",
-            alg_params,
+            {
+                "ASCENDING": True,
+                "EXPRESSION": "distance",
+                "INPUT": outputs["RetainFields"]["OUTPUT"],
+                "NULLS_FIRST": False,
+                "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
+            },
             context=context,
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["OrderByExpression"] is not None
 
         feedback.setCurrentStep(5)
         if feedback.isCanceled():
             return {}
+        feedback.setProgressText("Features ordered by distance.")
 
         # Extract by expression - international
-        alg_params = {
-            "EXPRESSION": "DesignationType = 'International'",
-            "INPUT": outputs["OrderByExpression"]["OUTPUT"],
-            "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
-        }
         outputs["ExtractByExpressionInternational"] = processing.run(
             "native:extractbyexpression",
-            alg_params,
+            {
+                "EXPRESSION": "DesignationType = 'International'",
+                "INPUT": outputs["OrderByExpression"]["OUTPUT"],
+                "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
+            },
             context=context,
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["ExtractByExpressionInternational"] is not None
 
         feedback.setCurrentStep(6)
         if feedback.isCanceled():
             return {}
+        feedback.setProgressText("International sites extracted.")
 
         # Drop field(s) - international
-        alg_params = {
-            "COLUMN": QgsExpression("'DesignationType'").evaluate(),
-            "INPUT": outputs["ExtractByExpressionInternational"]["OUTPUT"],
-            "OUTPUT": parameters["International"],
-        }
         outputs["DropFieldsInternational"] = processing.run(
             "native:deletecolumn",
-            alg_params,
+            {
+                "COLUMN": QgsExpression("'DesignationType'").evaluate(),
+                "INPUT": outputs["ExtractByExpressionInternational"]["OUTPUT"],
+                "OUTPUT": parameters["International"],
+            },
             context=context,
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["DropFieldsInternational"] is not None
         results["International"] = outputs["DropFieldsInternational"]["OUTPUT"]
+        context.layerToLoadOnCompletionDetails(results["International"]).name = (
+            "International"
+        )
 
         feedback.setCurrentStep(7)
         if feedback.isCanceled():
             return {}
+        feedback.setProgressText("Unnecessary fields dropped.")
 
         # Extract by expression - national
-        alg_params = {
-            "EXPRESSION": "DesignationType = 'National and Local'",
-            "INPUT": outputs["OrderByExpression"]["OUTPUT"],
-            "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
-        }
         outputs["ExtractByExpressionNational"] = processing.run(
             "native:extractbyexpression",
-            alg_params,
+            {
+                "EXPRESSION": "DesignationType = 'National and Local'",
+                "INPUT": outputs["OrderByExpression"]["OUTPUT"],
+                "OUTPUT": QgsProcessing.TEMPORARY_OUTPUT,
+            },
             context=context,
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["ExtractByExpressionNational"] is not None
 
         feedback.setCurrentStep(8)
         if feedback.isCanceled():
             return {}
+        feedback.setProgressText("National sites extracted.")
 
         # Drop field(s) - national
-        alg_params = {
-            "COLUMN": QgsExpression("'DesignationType'").evaluate(),
-            "INPUT": outputs["ExtractByExpressionNational"]["OUTPUT"],
-            "OUTPUT": parameters["NationalAndLocal"],
-        }
         outputs["DropFieldsNational"] = processing.run(
             "native:deletecolumn",
-            alg_params,
+            {
+                "COLUMN": QgsExpression("'DesignationType'").evaluate(),
+                "INPUT": outputs["ExtractByExpressionNational"]["OUTPUT"],
+                "OUTPUT": parameters["NationalAndLocal"],
+            },
             context=context,
             feedback=feedback,
             is_child_algorithm=True,
         )
+
+        assert outputs["DropFieldsNational"] is not None
+
         results["NationalAndLocal"] = outputs["DropFieldsNational"]["OUTPUT"]
+        context.layerToLoadOnCompletionDetails(results["NationalAndLocal"]).name = (
+            "NationalAndLocal"
+        )
+
+        feedback.setProgressText("Unnecessary fields dropped.")
+
         return results
 
     def name(self) -> str:
