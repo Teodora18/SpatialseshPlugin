a33cdeb8c59929070d592d344fe2c80be85383d7
Teodora Koleva <tedi20021810@gmail.com>
2026-09-21 13:54:38 +0300

    Init algs

---
+++ SpatialseshPlugin/algorithms/__init__.py
@@ -0,0 +1,4 @@
+from .DistanceAndBearing import DistanceAndBearingAlgorithm
+from .DesignatedSite_DistanceAndBearing import DesignatedSite_DistanceAndBearingAlgorithm
+from .Fix_layer_general import Fix_layer_general
+from .BNG_fix_layer import BNG_FixLayerAlgorithm
